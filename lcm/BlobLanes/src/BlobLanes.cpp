c40c66b9dcae4ecbee1b65e968f5f6877d80075e
R Fish <fishr@mit.edu>
2013-03-24 21:36:29 -0400

    new lane finding based on lane color transitions

---
+++ lcm/BlobLanes/src/BlobLanes.cpp
@@ -0,0 +1,193 @@
+//============================================================================
+// Name        : BlobLanes.cpp
+// Author      : Ryan Fish
+// Version     :
+// Copyright   : MIT License
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "opencv/cv.h"
+#include "opencv/cvaux.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <stdio.h>
+using namespace std;
+using namespace cv;
+
+Mat src, blurred, src_gray, edges, element, denoised_img, lanes;
+
+int min_threshold = 50;
+int max_trackbar = 200;
+int blacktowhite = 0;
+
+const char* standard_name = "Standard Hough Lines Demo";
+const char* probabilistic_name = "Probabilistic Hough Lines Demo";
+
+int p_trackbar = 116;
+
+int smoothfactor=20;
+
+int xup =16;
+int yup =0;
+int xdown =3;
+int ydown =3;
+
+
+int expectedLaneWidth = 40;
+
+void blacknwhite(int, void*);
+void denoise(int, void*);
+void findLanes();
+
+int main() {
+
+	/// Read the image
+	src = imread( "/home/blicycle/Desktop/blicycle/lcm/LineDetection/Debug/track_curved_hash.jpg", 1 );
+
+	src.adjustROI(-300, 0, 0, 0 );
+
+
+	/// Pass the image to gray
+	cvtColor( src, src_gray, CV_RGB2GRAY );
+
+	///blur for comedic effect
+	//blur( src, blurred, Size( 3, 5 ), Point(-1,-1) );
+	pyrDown(src_gray, blurred);
+	namedWindow("blurred", CV_WINDOW_AUTOSIZE);
+	imshow("blurred", blurred);
+
+
+	/// Apply Canny edge detector
+	//Canny( src_gray, edges, 100, 300, 3 );
+	threshold(blurred, edges, p_trackbar, 255, THRESH_BINARY );
+	namedWindow("gray", CV_WINDOW_AUTOSIZE);
+	imshow("gray", edges);
+
+	element = getStructuringElement(MORPH_RECT, Size(xup+1, yup+1), Point(-1,-1));
+	dilate(edges, denoised_img, element);
+	element = getStructuringElement(MORPH_RECT, Size(xdown+1, ydown+1), Point(-1,-1));
+	erode(denoised_img, denoised_img, element);
+	namedWindow("denoise", CV_WINDOW_AUTOSIZE);
+	imshow("denoise", denoised_img);
+
+	/// Create Trackbars for Thresholds
+	char thresh_label[50];
+	sprintf( thresh_label, "Thres: %d + input", min_threshold );
+
+	//namedWindow( standard_name, CV_WINDOW_AUTOSIZE );
+	//createTrackbar( thresh_label, standard_name, &s_trackbar, max_trackbar, Standard_Hough);
+
+	createTrackbar( thresh_label, "gray", &p_trackbar, max_trackbar, &blacknwhite);
+	createTrackbar( "Inv?", "gray", &blacktowhite, 1, &blacknwhite);
+	createTrackbar( "xup", "denoise", &xup, smoothfactor, &denoise);
+	createTrackbar( "yup", "denoise", &yup, smoothfactor, &denoise);
+	createTrackbar( "xdown", "denoise", &xdown, smoothfactor, &denoise);
+	createTrackbar( "ydown", "denoise", &ydown, smoothfactor, &denoise);
+
+	waitKey(0);
+
+
+	findLanes();
+	return 0;
+}
+
+void blacknwhite(int, void*){
+	if(blacktowhite!=true){
+		///try filtering out the white since we have lines anyway
+		threshold(blurred, edges, p_trackbar, 255, THRESH_BINARY );
+		imshow("gray", edges);
+	}else{
+		///try filtering out the black since we have lines anyway
+		threshold(blurred, edges, p_trackbar, 255, THRESH_BINARY_INV );
+		imshow("gray", edges);
+	}
+}
+
+void denoise(int, void*){
+	element = getStructuringElement(MORPH_RECT, Size(xup+1, yup+1), Point(-1,-1));
+	dilate(edges, denoised_img, element);
+	element = getStructuringElement(MORPH_RECT, Size(xdown+1, ydown+1), Point(-1,-1));
+	erode(denoised_img, denoised_img, element);
+	imshow("denoise", denoised_img);
+}
+
+void laneCenters(Mat rowIn, int* rowDeriv, int* rowLines){
+	Mat lowerRow = denoised_img.row(denoised_img.rows-10);
+		int lastPixel=0;
+		unsigned int nDeriv=0;
+		unsigned int nLines =0;
+		for(int i =0; i<rowIn.cols; i++){
+			uchar* pixel = lowerRow.ptr(0);
+			if(pixel[i]!=lastPixel){
+				lastPixel =  pixel[i];
+				nDeriv++;
+				rowDeriv[nDeriv]=i;
+				printf("got low transition!  %i, %i  \n", i, lastPixel);
+
+				if((i!=0)&&(i-rowDeriv[nDeriv-1]>80)){
+					nLines++;
+					rowLines[nLines]=(i+rowDeriv[nDeriv-1])/2;
+					circle(lanes, Point(rowLines[nLines], edges.rows-10),5,120);
+				}
+			}
+
+		}
+}
+
+void findLanes(){
+	denoised_img.copyTo(lanes);
+	Mat lowerRow = denoised_img.row(denoised_img.rows-10);
+	int lowerRowDeriv[lowerRow.cols];
+	int lastPixel=0;
+	unsigned int nDeriv=0;
+	int lowerRowLines[lowerRow.cols];
+
+	laneCenters(lowerRow, lowerRowDeriv, lowerRowLines);
+
+	unsigned int nLines =0;
+	for(int i =0; i<lowerRow.cols; i++){
+		uchar* pixel = lowerRow.ptr(0);
+		if(pixel[i]!=lastPixel){
+			lastPixel =  pixel[i];
+			nDeriv++;
+			lowerRowDeriv[nDeriv]=i;
+			printf("got low transition!  %i, %i  \n", i, lastPixel);
+
+			if((i!=0)&&(i-lowerRowDeriv[nDeriv-1]>80)){
+				nLines++;
+				lowerRowLines[nLines]=(i+lowerRowDeriv[nDeriv-1])/2;
+				circle(lanes, Point(lowerRowLines[nLines], edges.rows-10),5,120);
+			}
+		}
+
+	}
+
+	Mat midRow = denoised_img.row(denoised_img.rows/3);
+	int midRowDeriv[midRow.cols];
+	lastPixel=0;
+	nDeriv=0;
+	nLines=0;
+	int midRowLines[midRow.cols];
+	for(int i =0; i<midRow.cols; i++){
+		uchar* pixel = midRow.ptr(0);
+		if(pixel[i]!=lastPixel){
+			lastPixel =  pixel[i];
+			nDeriv++;
+			midRowDeriv[nDeriv]=i;
+			printf("got mid transition!  %i, %i  \n", i, lastPixel);
+			if((i!=0)&&(i-midRowDeriv[nDeriv-1]>60)){
+							nLines++;
+							midRowLines[nLines]=(i+midRowDeriv[nDeriv-1])/2;
+							circle(lanes, Point(midRowLines[nLines], edges.rows/2),5,120);
+
+						}
+		}
+	}
+
+	namedWindow("lanes", CV_WINDOW_AUTOSIZE);
+	imshow("lanes", lanes);
+
+
+	waitKey(0);
+}
--- lcm/LineDetection/LineDetection.cpp
+++ lcm/LineDetection/LineDetection.cpp
@@ -61,7 +61,7 @@ int main( int, char** argv )
    src.adjustROI(-300, 0, 0, 0 );
 
    ///blur for comedic effect
-   blur( src, blurred, Size( 2, 6 ), Point(-1,-1) );
+   blur( src, blurred, Size( 3, 5 ), Point(-1,-1) );
    namedWindow("blurred", CV_WINDOW_AUTOSIZE);
    imshow("blurred", blurred);
 
@@ -69,20 +69,26 @@ int main( int, char** argv )
    cvtColor( blurred, src_gray, CV_RGB2GRAY );
 
    /// Apply Canny edge detector
-   Canny( src_gray, edges, 100, 300, 3 );
+   //Canny( src_gray, edges, 100, 300, 3 );
+
+   ///try filtering out the white since we have lines anyway
+   threshold(src_gray, edges, 100, 255, THRESH_BINARY );
+   namedWindow("gray", CV_WINDOW_AUTOSIZE);
+   imshow("gray", edges);
 
    /// Create Trackbars for Thresholds
    char thresh_label[50];
    sprintf( thresh_label, "Thres: %d + input", min_threshold );
 
-   namedWindow( standard_name, CV_WINDOW_AUTOSIZE );
-   createTrackbar( thresh_label, standard_name, &s_trackbar, max_trackbar, Standard_Hough);
+   //namedWindow( standard_name, CV_WINDOW_AUTOSIZE );
+   //createTrackbar( thresh_label, standard_name, &s_trackbar, max_trackbar, Standard_Hough);
 
    namedWindow( probabilistic_name, CV_WINDOW_AUTOSIZE );
    createTrackbar( thresh_label, probabilistic_name, &p_trackbar, max_trackbar, Probabilistic_Hough);
 
    /// Initialize
-   Standard_Hough(0, 0);
+   //Standard_Hough(0, 0); //it detects too many lines, the other one places preference
+   	   	   	   	   	   	   //for more lines with there already are some, good!!
    Probabilistic_Hough(0, 0);
    waitKey(0);
    return 0;
@@ -97,6 +103,18 @@ void help()
   printf("\t Hough Transform to detect lines \n ");
   printf("\t---------------------------------\n ");
   printf(" Usage: ./HoughLines_Demo <image_name> \n");
+}
+
+int sort_lower_x(const void *a, const void *b)
+{
+	Vec<int,4> *Va = (Vec<int, 4> *) a;
+	Vec<int,4> *Vb = (Vec<int, 4> *) b;
+	//printf("%i \n", Va->val[0]);
+	if ((Va->val[0])>(Vb->val[0])){
+		return 1;
+	}else{
+		return -1;
+	}
 }
 
 /**
@@ -135,12 +153,95 @@ void Probabilistic_Hough( int, void* )
   cvtColor( edges, probabilistic_hough, CV_GRAY2BGR );
 
   /// 2. Use Probabilistic Hough Transform
-  HoughLinesP( edges, p_lines, 1, CV_PI/180, min_threshold + p_trackbar, 30, 10 );
+  HoughLinesP( edges, p_lines, 1, CV_PI/180, min_threshold + p_trackbar, 30, 200);
+  //I patched the probability based Hough Lines in opencv/modules/imgproc/hough.cpp
+  //so that the output vectors will always have the lesser y value as their first y value
+  /**************************************************************************************
+   * if( good_line )
+        {
+	CvRect lr;
+	    if(line_end[0].y<line_end[1].y){
+        	CvRect lr = { line_end[0].x, line_end[0].y, line_end[1].x, line_end[1].y };
+	    }else{
+		CvRect lr = { line_end[1].x, line_end[1].y, line_end[0].x, line_end[0].y };
+	    }
+            cvSeqPush( lines, &lr );
+            if( lines->total >= linesMax )
+                return;
+        }
+   **************************************************************************************/
+
+
+  //Sort by the lower X value to sort into similar line beginnings
+  qsort(p_lines.data(), p_lines.size(), sizeof(Vec4i), sort_lower_x);
+
+  vector<Vec4i> lowerXs(p_lines.size()/2);
+  lowerXs[0] = p_lines[0];
+
+  for( size_t i = 1; i<p_lines.size()/2; i++ )
+     {
+	  int offset=0;
+	  if (abs(lowerXs[i-1-offset][0]-p_lines[i*2][0])>70)
+	  {
+		  lowerXs[i-offset] = p_lines[i];
+	  }else{
+		  if (abs(lowerXs[i-1-offset][2]-p_lines[i*2][2])>70){
+			  lowerXs[i-offset] = p_lines[i*2];
+		  }else{
+			  offset++;
+
+			  lowerXs[i-offset][0] = (lowerXs[i-1][0]+p_lines[i*2][0])/2;
+			  lowerXs[i-offset][1] = min(lowerXs[i-1][1], p_lines[i*2][1]);
+			  lowerXs[i-offset][2] = (lowerXs[i-1][2]+p_lines[i*2][2])/2;
+			  lowerXs[i-offset][3] = max(lowerXs[i-1][3], p_lines[i*2][3]);
+
+		  }
+	  }
+
+     }
+  Vec4i testing;
+  testing[3]=0;
+  lowerXs.push_back(testing);
+
+  while(lowerXs.back()[3]==0){
+	  lowerXs.pop_back();
+	  printf("%i", (int) lowerXs.size());
+  }
+
+  qsort(lowerXs.data(), lowerXs.size(), sizeof(Vec4i), sort_lower_x);
+
+  vector<Vec4i> reducedLines(lowerXs.size());
+  reducedLines[0] = lowerXs[0];
+
+  for( size_t i = 1; i<lowerXs.size(); i++ )
+     {
+	  if (abs(reducedLines[i-1][0]-lowerXs[i][0])>50)
+	  {
+		  reducedLines[i] = lowerXs[i];
+	  }else{
+		  if (abs(reducedLines[i-1][2]-lowerXs[i][2])>50){
+			  reducedLines[i] = lowerXs[i];
+		  }else{
+			  reducedLines[i-1][0] = 0;
+			  reducedLines[i-1][1] = 0;
+			  reducedLines[i-1][2] = 0;
+			  reducedLines[i-1][3] = 0;
+
+			  reducedLines[i][0] = (reducedLines[i-1][0]+lowerXs[i][0])/2;
+			  reducedLines[i][1] = min(reducedLines[i-1][1], lowerXs[i][1]);
+			  reducedLines[i][2] = (reducedLines[i-1][2]+lowerXs[i][2])/2;
+			  reducedLines[i][3] = max(reducedLines[i-1][3], lowerXs[i][3]);
+
+		  }
+	  }
+
+     }
+
 
   /// Show the result
-  for( size_t i = 0; i < p_lines.size(); i++ )
+  for( size_t i = 0; i < reducedLines.size(); i++)//p_lines.size(); i++ )
      {
-       Vec4i l = p_lines[i];
+       Vec4i l = reducedLines[i];//p_lines[i];
        line( probabilistic_hough, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,0,0), 3, CV_AA);
      }
 
